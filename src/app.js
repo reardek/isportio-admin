1ccb153bc15de5d9b108cb4b0f7ed34f972f26bd
reardek <jjkowalinski@yahoo.com>
2020-09-15 18:36:45 +0200

    add search input

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { fade, makeStyles, useTheme } from "@material-ui/core/styles";
 import {
   AppBar,
   Toolbar,
@@ -11,8 +11,9 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  InputBase,
 } from "@material-ui/core";
-import { Menu, ChevronLeft, ChevronRight } from "@material-ui/icons";
+import { Menu, ChevronLeft, ChevronRight, Search } from "@material-ui/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
 import clsx from "clsx";
@@ -67,6 +68,43 @@ const useStyles = makeStyles((theme) => ({
       width: theme.spacing(9) + 1,
     },
   },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
   toolbar: {
     display: "flex",
     alignItems: "center",
@@ -115,8 +153,21 @@ export default function AppAdmin() {
             <Menu />
           </IconButton>
           <Typography variant="h6" noWrap>
-            Mini variant drawer
+            iSportio Admin Panel
           </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <Search />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
         </Toolbar>
       </AppBar>
       <Drawer
